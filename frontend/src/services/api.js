cd6056c81989fed285125ec926e5b3c5a235f3ff
kyle <kylewhat@gmail.com>
2025-10-31 10:08:21 -0500

    change frontend to defaul port

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL?.replace(/\/+$/, "") || "http://localhost:8001/api",
+  baseURL: import.meta.env.VITE_API_URL?.replace(/\/+$/, "") || "http://localhost:3000/api",
   withCredentials: false,
   timeout: 15000,
 });
